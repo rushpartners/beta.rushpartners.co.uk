a3d4ea364534a6d6e261d3d8eeebd6cf99616a51
Simon Stack <simon@rushpartners.co.uk>
2018-07-16 16:05:33 +0100

    Home styling WIP

--- source/_partials/our-brands.blade.php
+++ source/_partials/our-brands.blade.php
@@ -1,4 +1,4 @@
-<section class="m-12 relative z-10 block-dark">
+<section class="m-20 relative z-10 block-dark">
   <h2 class="h2 h2--right">
     Our brands
   </h2>
@@ -23,57 +23,78 @@
   </div>
 </section>
 
-<section class="passion mt-12">
-  <h2 class="h2">
+<section class="home__passion relative clearfix my-32 px-20 border-box">
+  <h2 class="h2 text-right float-right">
     Driven by passion
   </h2>
 
-  <div class="block-dark">
+  <div class="block-dark lg:w-1/2 lg:float-right clear">
     <h3 class="h3">Working on creating better user experiences</h3>
 
     <p>After years of working on defining different products for multiple sports and markets, our aim is to improve the experience of products that need to adapt to the current times and markets.</p>
   </div>
 </section>
 
-<section class="passion">
-  <h2 class="h2 h2--right">
-    With the user always in mind
+<section class="home__user-in-mind p-20 mt-32 clearfix">
+  <h2 class="h2 h2--right lg:float-left">
+    <span class="lg:block">With the user</span> <span class="lg:block">always in mind</span>
   </h2>
 
-  <div class="block-dark">
+  <div class="block-dark lg:w-1/2 lg:float-left clear">
     <h3 class="h3">Adapting old markets to new technologies</h3>
 
     <p>In a world where we want to have everything on our hands, it is time to adapt both product and mentality to improve our products for the current needs.</p>
   </div>
 </section>
 
-<section class="team">
-  <h2 class="h2">
-    With the user always in mind
+<section class="home__team clearfix">
+  <h2 class="h2 h2--right">
+    Meet the team
   </h2>
 
-  <div class="block-dark">
+  <div class="block-dark lg:w-1/2 lg:float-right clear">
     <h3 class="h3">Talent aimed for disruption</h3>
 
     <p>We are a team of people from all over the world with the same vision and objective of changing something that we believe is fundamentally flawed and using the latest technology to do it.</p>
   </div>
 </section>
 
-<section class="work">
-  <h2 class="h2">
+<section class="home__work block-dark my-32 p-20">
+  <h2 class="h2 h2--right h2--blue">
     Work with us
   </h2>
 
-  <p>If you share our vision and are willing to accept a new challenge, check below if there's something that could suit you and get in touch!</p>
+  <p class="lg:w-1/2 lg:float-right lg:text-right">If you share our vision and are willing to accept a new challenge, check below if there's something that could suit you and get in touch!</p>
 
-  <ul>
+  <ul class="my-20">
     <li>
-      <strong>Junior Web Developer</strong>
-      <div>Engineering</div>
-      <div>London, England</div>
-      <a href="#" class="btn btn--cyan">Apply now</a>
+      <strong class="h3">Junior Web Developer</strong>
+      <div class="home__work-dept">Engineering</div>
+      <div class="home__work-location">London, England</div>
+      <div>
+        <a href="#" class="btn btn--cyan">Apply now</a>
+      </div>
+    </li>
+    <li>
+      <strong class="h3">Web Designer</strong>
+      <div class="home__work-dept">Engineering</div>
+      <div class="home__work-location">London, England</div>
+      <div>
+        <a href="#" class="btn btn--cyan">Apply now</a>
+      </div>
+    </li>
+    <li>
+      <strong class="h3">Data Analyst</strong>
+      <div class="home__work-dept">Engineering</div>
+      <div class="home__work-location">London, England</div>
+      <div>
+        <a href="#" class="btn btn--cyan">Apply now</a>
+      </div>
     </li>
   </ul>
 
-  <div>Didn't find anything that fits you? <a href="careers" class="btn btn--hollow">Contact us</a></div>
+  <div class="clearfix">
+    <span class="h3 float-left">Didn't find anything that fits you?</span>
+    <a href="careers" class="btn btn--hollow float-right">Contact us</a>
+  </div>
 </section>
