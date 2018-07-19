78984d075970628703413962a352b1a01d80aa7d
luisrush <41112181+luisrush@users.noreply.github.com>
2018-07-19 11:58:33 +0100

    Added favicon and meta description

--- source/_partials/home/brands.blade.php
+++ source/_partials/home/brands.blade.php
@@ -6,11 +6,11 @@
   <p class="xl:w-2/5 lg:w-3/5 md:text-right sm:text-right md:float-right clearfix">Our vision is shared by our brands, working together to revolutionize the industry from every market possible.</p>
 
   <ul class="home-brands__list">
-    <li><img src="images/brands/onebet.svg" alt="Onebet" /></li>
-    <li><img src="images/brands/redsbet.svg" alt="Redsbet" /></li>
-    <li><img src="images/brands/norskespill.svg" alt="Norskespill" /></li>
-    <li><img src="images/brands/redarmybet.svg" alt="Redarmybet" /></li>
-    <li><img src="images/brands/fansbet.svg" alt="Fansbet" /></li>
-    <li><img src="images/brands/fantasybet.svg" alt="FantasyBet" /></li>
+    <li><a href="https://www.onebet.com/home" target="_blank"><img src="images/brands/onebet.svg" alt="Onebet" /></a></li>
+    <li><a href="https://www.redsbet.com/home" target="_blank"><img src="images/brands/redsbet.svg" alt="Redsbet" /></a></li>
+    <li><a href="http://norskespill.com/" target="_blank"><img src="images/brands/norskespill.svg" alt="Norskespill" /></a></li>
+    <li><a href="https://www.redarmybet.com/" target="_blank"><img src="images/brands/redarmybet.svg" alt="Redarmybet" /></a></li>
+    <li><a href="https://www.fansbet.com/home" target="_blank"><img src="images/brands/fansbet.svg" alt="Fansbet" /></a></li>
+    <li><a href="https://www.fantasybet.com/" target="_blank"><img src="images/brands/fantasybet.svg" alt="FantasyBet" /></a></li>
   </ul>
 </section>
