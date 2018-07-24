0bb240767307ed5999ed181d0a34a1f4e8ea62a4
luisrush <41112181+luisrush@users.noreply.github.com>
2018-07-24 14:46:22 +0100

    Mobile navigation and responsiveness

--- source/_assets/js/main.js
+++ source/_assets/js/main.js
@@ -41,3 +41,25 @@ class Main {
 }
 
 module.exports = new Main();
+
+// Animate burger
+var forEach=function(t,o,r){if("[object Object]"===Object.prototype.toString.call(t))for(var c in t)Object.prototype.hasOwnProperty.call(t,c)&&o.call(r,t[c],c,t);else for(var e=0,l=t.length;l>e;e++)o.call(r,t[e],e,t)};
+
+var hamburgers = document.querySelectorAll(".hamburger");
+if (hamburgers.length > 0) {
+  forEach(hamburgers, function(hamburger) {
+    hamburger.addEventListener("click", function() {
+      this.classList.toggle("is-active");
+    }, false);
+  });
+};
+
+// Add class to parent navigation div
+  $(".hamburger").click(function (e) {
+          if($(".nav").hasClass("nav--active")) {
+     console.log("hereee")
+          $(".nav").removeClass("nav--active")
+        } else {
+         $(".nav").addClass("nav--active")
+        }
+   });
